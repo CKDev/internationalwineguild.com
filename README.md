23a89c41ec2f4e83a1acc4d37b1ef5abb857fde2
Alex Brinkman <abrink@commercekitchen.com>
2019-07-29 14:47:27 -0600

    [CHG] Fix absolute links

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Tynan's International Wine Guild site.
 ## TODO
 
 - Finish the wufoo forms
+- Redirects from server
+- Meta data and descriptions
--- javascripts/share-n-track.js
+++ javascripts/share-n-track.js
@@ -1,44 +1,39 @@
-	// Facebook async loading of JSAPI -->
-	(function() {
-		window.fbAsyncInit = function() { 
-			FB.init({ appId: '237778732920749', status: true, cookie: true, xfbml: true}); //
-		};
-	  	var e = document.createElement('script'); e.async = true;
-	  	e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js#xfbml=1';
-	    document.getElementById('fb-root').appendChild(e);
-	})();
- 	// Load Twitter JS-API asynchronously -->
-    (function(){
-      var twitterWidgets = document.createElement('script');
-      twitterWidgets.type = 'text/javascript';
-      twitterWidgets.async = true;
-      twitterWidgets.src = 'http://platform.twitter.com/widgets.js';
+// Facebook async loading of JSAPI -->
+(function() {
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId: "237778732920749",
+      status: true,
+      cookie: true,
+      xfbml: true
+    }); //
+  };
+  var e = document.createElement("script");
+  e.async = true;
+  e.src =
+    document.location.protocol + "//connect.facebook.net/en_US/all.js#xfbml=1";
+  document.getElementById("fb-root").appendChild(e);
+})();
+//Google plusone  -->
+(function() {
+  var po = document.createElement("script");
+  po.type = "text/javascript";
+  po.async = true;
+  po.src = "https://apis.google.com/js/plusone.js";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(po, s);
+})();
 
-      // Setup a callback to track once the script loads.
-      twitterWidgets.onload = _ga.trackTwitter;
+$(function() {
+  $("a.ga-track").click(function() {
+    if ($(this).attr("data-kpi")) {
+      _gaq.push(["_trackPageview", $(this).attr("data-kpi")]);
+    } else {
+      _gaq.push(["_trackPageview", $(this).attr("href")]);
+    }
+  });
 
-      document.getElementsByTagName('head')[0].appendChild(twitterWidgets);
-    })();
-	//Google plusone  -->
-  	(function() {
-    	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    	po.src = 'https://apis.google.com/js/plusone.js';
-    	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  	})();
-
-$(function(){
-	
-	$('a.ga-track').click(function(){
-		if ($(this).attr('data-kpi')) {
-			_gaq.push(['_trackPageview',$(this).attr('data-kpi')]);
-		} else {
-			_gaq.push(['_trackPageview',$(this).attr('href')]);
-		} 
-	});
-	
-	$('form.ga-track').submit(function(){
-		_gaq.push(['_trackPageview',$(this).attr('data-kpi')]);
-	});
-
-
+  $("form.ga-track").submit(function() {
+    _gaq.push(["_trackPageview", $(this).attr("data-kpi")]);
+  });
 });
